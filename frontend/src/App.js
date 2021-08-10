294012fd9d52ffba1d0ed5eaea31a0166b292cbf
Tushar Srivastava <tushar8590@gmail.com>
2021-08-10 18:42:18 +0530

    react app team

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { TeamPage } from './pages/teamPage';
+function App() {
+  return (
+    <div className="App">
+      <TeamPage />
+    </div>
+  );
+}
+
+export default App;
--- src/main/java/com/practicaljava/data/controller/TeamController.java
+++ src/main/java/com/practicaljava/data/controller/TeamController.java
@@ -5,11 +5,13 @@ import com.practicaljava.data.repository.TeamRepository;
 import com.practicaljava.model.Team;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
     
     @Autowired
